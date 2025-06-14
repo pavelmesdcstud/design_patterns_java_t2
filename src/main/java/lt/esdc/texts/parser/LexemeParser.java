093029d4677351847a166e0be2b9b53b83ed93a7
pavelmesdcstud <206596599+pavelmesdcstud@users.noreply.github.com>
2025-06-14 18:55:12 +0300

    feat: upd lexeme parser

--- src/main/java/lt/esdc/texts/parser/LexemeParser.java
+++ src/main/java/lt/esdc/texts/parser/LexemeParser.java
@@ -3,16 +3,22 @@ package lt.esdc.texts.parser;
 import lt.esdc.texts.composite.Lexeme;
 import lt.esdc.texts.composite.TextComponent;
 import lt.esdc.texts.exception.TextParseException;
+import lt.esdc.texts.interpreter.ArithmeticExpressionCalculator;
+import lt.esdc.texts.interpreter.BitwiseExpressionCalculator;
+import lt.esdc.texts.interpreter.ExpressionCalculator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class LexemeParser extends AbstractParser {
     private static final Logger logger = LogManager.getLogger(LexemeParser.class);
 
+    // Simple regex to identify potential expressions (contains numbers and operators)
     private static final String ARITHMETIC_EXPRESSION_REGEX = ".*[\\d].*[+\\-*/].*";
     private static final String BITWISE_EXPRESSION_REGEX = ".*[\\d].*[&|^~<>].*";
     private static final String PUNCTUATION_REGEX = "[.,!?'\"():;-]";
 
+    private final ExpressionCalculator arithmeticCalculator = new ArithmeticExpressionCalculator();
+    private final ExpressionCalculator bitwiseCalculator = new BitwiseExpressionCalculator();
 
     @Override
     public void parse(TextComponent component, String text) throws TextParseException {
@@ -20,7 +26,25 @@ public class LexemeParser extends AbstractParser {
         String wordPart = text.replaceAll(PUNCTUATION_REGEX, "");
         String punctuationPart = text.substring(wordPart.length());
 
-        if (!wordPart.isEmpty()){
+        if (wordPart.matches(BITWISE_EXPRESSION_REGEX)) {
+            try {
+                String result = bitwiseCalculator.calculate(wordPart);
+                component.add(Lexeme.expression(result));
+                logger.info("Calculated bitwise expression '{}' -> '{}'", wordPart, result);
+            } catch (TextParseException e) {
+                logger.warn("Could not parse bitwise expression: {}. Treating as word.", wordPart, e);
+                component.add(Lexeme.word(wordPart));
+            }
+        } else if (wordPart.matches(ARITHMETIC_EXPRESSION_REGEX)) {
+            try {
+                String result = arithmeticCalculator.calculate(wordPart);
+                component.add(Lexeme.expression(result));
+                logger.info("Calculated arithmetic expression '{}' -> '{}'", wordPart, result);
+            } catch (TextParseException e) {
+                logger.warn("Could not parse arithmetic expression: {}. Treating as word.", wordPart, e);
+                component.add(Lexeme.word(wordPart));
+            }
+        } else if (!wordPart.isEmpty()){
             component.add(Lexeme.word(wordPart));
         }
 
